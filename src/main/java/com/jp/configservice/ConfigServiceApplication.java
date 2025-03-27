3088d61860c38d93ca5797f7faf58fd1b9b24686
jaiminpatel7024 <jaiminpatel7024@yahoo.com>
2025-03-27 11:48:30 +0530

    Enable Eureka Server

--- src/main/java/com/jp/configservice/ConfigServiceApplication.java
+++ src/main/java/com/jp/configservice/ConfigServiceApplication.java
@@ -2,9 +2,12 @@ package com.jp.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServiceApplication {
 
