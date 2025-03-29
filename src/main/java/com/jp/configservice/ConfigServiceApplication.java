2335fad72962e69c1abcb9266e74d9121d873fb8
jaiminpatel7024 <jaiminpatel7024@yahoo.com>
2025-03-29 12:39:11 +0530

    Dynamic VM IP

--- src/main/java/com/jp/configservice/ConfigServiceApplication.java
+++ src/main/java/com/jp/configservice/ConfigServiceApplication.java
@@ -1,5 +1,6 @@
 package com.jp.configservice;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
